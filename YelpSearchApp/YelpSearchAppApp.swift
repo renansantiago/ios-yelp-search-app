ee39a8ce9ed1e95ebd657ddd8bd7048d52ad85cc
Renan Santiago <renan.santiagoo@hotmail.com>
2025-03-31 22:51:07 -0300

    chore: initial basic iOS project with SwiftUI

---
+++ YelpSearchApp/YelpSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpSearchAppApp.swift
+//  YelpSearchApp
+//
+//  Created by Renan on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct YelpSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
